10a0af0ef50bb22876820f13bb48682ef356734a
mihirp19 <96230061+mihirp19@users.noreply.github.com>
2022-08-03 12:31:44 +0530

    header js and css

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+
+
+const header = () => {
+  return (
+    <div className='header'>
+        <h2>This is a header💎</h2>
+        <div className='logo'>C R Y P S I O N</div>
+
+    <div className='searchBar'>
+        <div>
+            <input className='searchInput' placeholder='Search Here'/>
+        </div>        
+    </div>
+</div>
+  )
+}
+
+export default header;
